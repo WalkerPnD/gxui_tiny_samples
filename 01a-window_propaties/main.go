b33b67b7f2caba6a3ce40db9f302833cbf8afec0
Koji Carvalho <Walker@Marcio-no-MacBook-Air.local>
2015-04-21 14:05:10 -0300

    adjust for readability

--- 01a-window_propaties/main.go
+++ 01a-window_propaties/main.go
@@ -13,11 +13,10 @@ func appMain(driver gxui.Driver) {
 
 	// Back ground color
 	color := gxui.Yellow // same with Color{1.0, 1.0, 0.0, 1.0}
+
 	// Type and List are here: https://github.com/google/gxui/blob/master/color.go#L60
 	brush := gxui.CreateBrush(color)
 	window.SetBackgroundBrush(brush)
-
-	// Comming soon: window.SetPadding(math.Spacing{L: 10, T: 10, R: 10, B: 10})
 
 	window.OnClose(driver.Terminate)
 }
